2c7aec808a1daa0bcea4913aeaec17597a67b95c
Eric <eric.klein@lello.com.br>
2022-07-10 17:18:35 -0300

    Create service to acess the SOAP webservice and retrieve the info

---
+++ exercises/src/main/java/com/test/exercises/services/CountryService.java
@@ -0,0 +1,29 @@
+package com.test.exercises.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.test.exercises.soap_configuration.CountryClient;
+import com.test.utils.soap_methods.FullCountryInfoAllCountriesResponse;
+import com.test.utils.soap_methods.TCountryInfo;
+
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@Log4j2
+public class CountryService {
+    @Autowired CountryClient client;
+
+    public String getCurrencyISOCode(String countryName){
+        log.info("Acessing the SOAP webservice to get all countries info");
+        FullCountryInfoAllCountriesResponse res = client.getCountries();
+		List<TCountryInfo> list = res.getFullCountryInfoAllCountriesResult().getTCountryInfo();
+		Optional<TCountryInfo> find = list.stream().filter(l -> l.getSName().equals(countryName)).findFirst();
+        log.info("Filtering the given country list to get the Currency Country ISO code of: " + countryName);
+        return find.get().getSCurrencyISOCode();
+    }
+
+}
