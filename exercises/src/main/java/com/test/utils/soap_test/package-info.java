8b19f1086ce5aa685d8cd4754aa6ff73246567b3
Eric <eric.klein@lello.com.br>
2022-07-10 21:12:15 -0300

    Minimal changes

--- exercises/src/main/java/com/test/utils/soap_test/package-info.java
+++ exercises/src/main/java/com/test/utils/soap_test/package-info.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2022.07.10 às 05:14:33 PM GMT-03:00 
+// Gerado em: 2022.07.10 às 09:03:49 PM GMT-03:00 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://com/test/utils/soap_test", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
