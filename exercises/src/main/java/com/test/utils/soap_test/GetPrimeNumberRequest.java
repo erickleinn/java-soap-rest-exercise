c9dcda47fabde1f1fe29c74d93b4bfa559c735be
Eric <eric.klein@lello.com.br>
2022-07-14 11:11:23 -0300

    Minimal adjustments

--- exercises/src/main/java/com/test/utils/soap_test/GetPrimeNumberRequest.java
+++ exercises/src/main/java/com/test/utils/soap_test/GetPrimeNumberRequest.java
@@ -2,7 +2,7 @@
 // Este arquivo foi gerado pela Arquitetura JavaTM para Implementação de Referência (JAXB) de Bind XML, v2.2.7 
 // Consulte <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas as modificações neste arquivo serão perdidas após a recompilação do esquema de origem. 
-// Gerado em: 2022.07.11 às 08:06:29 PM GMT-03:00 
+// Gerado em: 2022.07.14 às 11:06:59 AM GMT-03:00 
 //
 
 
